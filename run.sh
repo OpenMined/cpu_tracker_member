07a65a6b6e413d68125be620fbcb399905ad8f20
Ionésio Junior <ionesiojr@gmail.com>
2024-11-14 17:48:35 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,17 +5,17 @@ if [ ! -d ".venv" ]; then
     echo "Virtual environment not found. Creating one..."
     uv venv .venv
     echo "Virtual environment created successfully."
-    uv pip install -U syftbox
     uv pip install -r requirements.txt
 else
     echo "Virtual environment already exists."
 fi
 
+uv pip install -U syftbox
 . .venv/bin/activate
 
 # # run app using python from venv
 echo "Running ftop with $(python3 --version) at '$(which python3)'"
-uv run python3 main.py
+python3 main.py
 
 # # deactivate the virtual environment
 deactivate
