b4a365ce699f87f8eb20da0d324980712e1bf428
Yash Gorana <yash.gorana@hotmail.com>
2025-03-17 13:59:54 +0530

    run.sh - setup once

--- run.sh
+++ run.sh
@@ -1,24 +1,17 @@
+#!/bin/sh
+set -e
+
+# setup once
+uv venv -p 3.12 .venv
+uv pip install -U -r requirements.txt
+. .venv/bin/activate
+
 while true; do
-    if [ ! -d ".venv" ]; then
-        echo "Virtual environment not found. Creating one..."
-        uv venv -p 3.12 .venv
-        echo "Virtual environment created successfully."
-        uv pip install -r requirements.txt
-    else
-        echo "Virtual environment already exists."
-    fi
-
-    uv pip install -U syftbox
-
-    . .venv/bin/activate
-
     echo "Running 'cpu_tracker_member' with $(python3 --version) at '$(which python3)'"
     uv run python3 main.py
-
-    deactivate
 
     echo "Sleeping for 10 seconds..."
     sleep 10
 done
 
-
+deactivate
